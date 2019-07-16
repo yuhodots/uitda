026189414adb0e113be978a53d4d1a160b8d003f
yuho8437 <yuho8437@unist.ac.kr>
2019-07-16 15:03:10 +0900

    manage setting

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var sanitizeHtml = require('sanitize-html');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title : 'Uitda'});
 });
 
+/* GET market page. */
 router.get('/market', function(req, res, next) {
-  res.render('market');
+  res.render('market/home');
 });
 
+/* GET networking page. */
 router.get('/networking', function(req, res, next) {
-  res.render('networking');
+  res.render('networking/home');
 });
 
+/* GET carpool page. */
 router.get('/carpool', function(req, res, next) {
-  res.render('carpool');
+  res.render('carpool/home');
 });
 
+/* GET manage page. */
 router.get('/manage', function(req, res, next) {
-  res.render('manage');
+  res.render('manage/home');
 });
 
 router.get('/manage/market-posts', function(req, res, next) {
-  res.render('manage_market-posts');
+  res.render('manage/market-posts');
 });
 
 router.get('/manage/networking-posts', function(req, res, next) {
-  res.render('manage_networking-posts');
+  res.render('manage/networking-posts');
 });
 
 router.get('/manage/carpool-posts', function(req, res, next) {
-  res.render('manage_carpool-posts');
+  res.render('manage/carpool-posts');
 });
 
 router.get('/manage/proposal', function(req, res, next) {
-  res.render('manage_proposal');
+  res.render('manage/proposal');
 });
 
 router.get('/manage/leave', function(req, res, next) {
-  res.render('manage_leave');
+  res.render('manage/leave');
 });
 
+/* GET chatting page. */
 router.get('/chatting', function(req, res, next) {
-  res.render('chatting');
+  res.render('chatting/home');
 });
 
 module.exports = router;
