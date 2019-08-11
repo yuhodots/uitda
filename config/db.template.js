5951f98634519058784b8ae1fac22e1c6ab6e707
yuho8437 <yuho8437@unist.ac.kr>
2019-08-11 12:19:17 +0900

    Update db.template.js

--- config/db.template.js
+++ config/db.template.js
@@ -16,7 +16,7 @@ function handleDisconnect() {
   db.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
+      return handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
     }
