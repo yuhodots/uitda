45b8e364a22cb6fe8c7e0409d4de8eb546545304
yuho8437 <yuho8437@unist.ac.kr>
2019-08-11 12:11:14 +0900

    Update db.template.js

--- config/db.template.js
+++ config/db.template.js
@@ -5,5 +5,23 @@ var db = mysql.createConnection({
   password:'',
   database:''
 });
-db.connect();
+function handleDisconnect() {
+  db.connect(function(err) {              // The server is either down
+    if(err) {                             // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  db.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
+
 module.exports = db;
