0bb4e82544bd23845a67c4fda8ea0deda8c41374
Soogeun Park <bwmelon97@unist.ac.kr>
2019-12-05 00:03:46 +0900

    styles/variables 색상 변경 및 추가

--- frontend/src/styles/variables.js
+++ frontend/src/styles/variables.js
@@ -1,6 +1,10 @@
 export const colors = {
     blue: '#1E90FF',
     gray_bg: '#F5F6F7',
-    gray_fontColor: '#888888',
+    gray_line: '#bec4c6',
     white: '#FFFFFF',
+
+    /* font colors */
+    font_gray: '#888888',
+    font_darkgray: '#444b4f'
 }
