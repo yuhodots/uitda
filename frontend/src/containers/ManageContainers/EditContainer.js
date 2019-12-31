28181bbc87d8ee1972355a3fa245eb88c086e3c7
Soogeun Park <bwmelon97@unist.ac.kr>
2019-12-31 18:37:06 +0900

    module updates

--- frontend/src/containers/ManageContainers/EditContainer.js
+++ frontend/src/containers/ManageContainers/EditContainer.js
@@ -1,6 +1,10 @@
 // 상위 컴포넌트: /pages/manage.js
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Header } from '../../components/Manage/ManageStructure'
+import Body from '../../components/Manage/ManageEdit'
 
 
 class EditContainer extends Component {
@@ -8,21 +12,20 @@ class EditContainer extends Component {
     render() {
         return(
             <div>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                Hello, world!
-                This is EditContainer :)
+                <Header isEdit={true} />
+                <Body />
             </div>
         )
     }
 
 }
 
+EditContainer.propTypes = {
+    isNew: PropTypes.bool,      // 새로 작성하는 것 인지
+}
+
+EditContainer.defaultProps = {
+    isNew: false
+}
 
 export default EditContainer;
