97fc06e520574db38892fb1b506f34aeba5b44d2
Soogeun Park <bwmelon97@unist.ac.kr>
2019-12-31 16:52:26 +0900

    Link Template과 배경 Template 추가

--- frontend/src/styles/templates/manage.js
+++ frontend/src/styles/templates/manage.js
@@ -3,6 +3,7 @@
 /* Manage 관련 styled component template 파일 */
 
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import { colors } from '../variables';
 
@@ -11,4 +12,19 @@ import { colors } from '../variables';
 export const BoxTemplate = styled.div`
     background-color: ${colors.white};
     border: 1px solid ${colors.gray_line};
+`;
+
+/* Link 태그를 상속하기 위해 만든 탬플릿 */
+export const LinkBoxTemplate = styled(Link)`
+    background-color: ${colors.white};
+    border: 1px solid ${colors.gray_line};
+
+    text-decoration: none;
+`;
+
+/* Body의 배경 템플릿
+   배경색, 최소 높이길이 설정 */
+export const BGTemplate = styled.div`
+    background-color: ${colors.gray_bg};
+    min-height: 40rem;
 `;
