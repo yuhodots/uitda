e9d71c83182dbe2e1a7d525f815618ec500c7424
Soogeun Park <bwmelon97@unist.ac.kr>
2020-01-11 18:58:51 +0900

    Board Search Icon 디자인 개선 및 오래된 코드 개선

--- frontend/src/components/Structure/SearchIcon/SearchIcon.js
+++ frontend/src/components/Structure/SearchIcon/SearchIcon.js
@@ -3,21 +3,63 @@
 // 상위 컴포넌트: MarketContainer, NetworkContainer
 
 import React from "react";
+import styled, { css } from 'styled-components';
+import { Icon } from 'antd';
 
-import './SearchIcon.css';
+import { colors } from '../../../styles/variables';
 
+
+/* Styled Components */
+
+/* 아이콘 영역
+   isHeaderOn 일 때는 visible: hidden이고,
+   반대의 경우 visible한 상탳이다.  */
+const CircleArea = styled.div`
+    width: 3.5rem;
+    height: 3.5rem;
+    position: fixed;
+    bottom: 2rem;
+    right: 2rem;
+    border-radius: 50%;
+    background-color: ${colors.blue};
+    cursor: pointer;
+
+    /* 세로 가로 가운데 정렬 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    /* props에 따른 visible 및 opacity 값 */
+    opacity: 0.85;
+    visibility: visible;
+
+    ${props =>
+      props.isHeaderOn && 
+      css`
+        opacity: 0;
+        visibility: 'hidden';
+      `};
+
+    transition: opacity 0.3s;
+`;
+
+const StyledIcon = styled(Icon)`
+    color: ${colors.white};
+    font-size: 1.25rem;
+    font-weight: bold;
+    opacity: 1;
+`;
+
+
+/* React Compoent */
 const SearchIcon = ({isHeaderOn, headerOn}) => {
     return (
-        <div 
-            className={
-                isHeaderOn ?
-                "SearchIcon hidden" :
-                "SearchIcon"
-            }
-
-            onClick={headerOn}>
-            검색
-        </div>
+        <CircleArea
+            isHeaderOn={isHeaderOn}
+            onClick={headerOn}
+        >
+            <StyledIcon type="search" />
+        </CircleArea>
     )
 }
 
