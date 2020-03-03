8babf98943777b78a3fb2858b9f375dee092aeba
Soogeun Park <bwmelon97@naver.com>
2020-03-03 20:56:27 +0900

    Create edit_RoomInfo_DataKeys.js

---
+++ frontend/src/constants/edit_RoomInfo_DataKeys.js
@@ -0,0 +1,7 @@
+export const TITLE = 'title';
+export const DEPARTURE = 'departure';
+export const DESTINATION = 'destination';
+export const START_TIME = 'start_time';
+export const MEETING_PALCE = 'meeting_place';
+export const CONTACT = 'contact';
+export const DESCRIPTION = 'description';
