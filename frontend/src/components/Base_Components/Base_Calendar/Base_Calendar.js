164f734a3763a808e5e849fa56bc49f98d178a5a
Soogeun Park <bwmelon97@naver.com>
2020-03-13 20:13:14 +0900

    캘린더 크기에 따라 한 칸에 들어가는 이벤트 개수 설정
    노트북: 3, 데스크탑: 4

--- frontend/src/components/Base_Components/Base_Calendar/Base_Calendar.js
+++ frontend/src/components/Base_Components/Base_Calendar/Base_Calendar.js
@@ -40,6 +40,8 @@ class Calendar extends Component {
                 dayHeads[i].addEventListener('mouseleave', (e) => this._handleMouseLeaveDayHead(e, dayElbg) )
             }
         })
+        window.addEventListener('resize', this._updateInnerWidth);
+        this._updateInnerWidth();
     }
 
     componentWillUnmount () {
@@ -53,6 +55,14 @@ class Calendar extends Component {
                 dayHeads[i].removeEventListener('mouseover', (e) => this._handleHoverDayHead(e, dayElbg) )
                 dayHeads[i].removeEventListener('mouseleave', (e) => this._handleMouseLeaveDayHead(e, dayElbg) )
             }
+        })
+        window.removeEventListener('resize', this._updateInnerWidth);
+    }
+
+    _updateInnerWidth = (e) => {
+        this.setState({
+            ...this.state,
+            innerWidth: window.innerWidth
         })
     }
 
@@ -184,6 +194,9 @@ class Calendar extends Component {
             eventsObj,
         } = this.props;
 
+        const { innerWidth } = this.state;
+        const isDeskTopDisplay = innerWidth > 1500;
+
         const events = this._dataObjToviewObjList(eventsObj);
 
         let props = ( category === MANAGE ?
@@ -224,7 +237,7 @@ class Calendar extends Component {
             fixedWeekCount: false,                              // 달력이 해당 월의 week 수 만큼 render됨 (6주 fix X)
 
             defaultTimedEventDuration: '00:00',                 // 카풀 이벤트의 duration을 없앰으로써 한 이벤트가 한 칸만 차지하도록 함
-            eventLimit: 4,                                      // 달력 한 칸에 나타나는 이벤트 최대 개수
+            eventLimit: isDeskTopDisplay ? 4 : 3,               // 달력 한 칸에 나타나는 이벤트 최대 개수
 
             dateClick: this._selectDate,                        // 달력의 날짜 부분 클릭 시 실행되는 함수
             eventClick: this._handleClickEvent,                 // 이벤트 클릭을 처리하는 함수
