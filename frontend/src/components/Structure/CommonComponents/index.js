9f3cbec1d55e1e089401406d111dad4abf60214e
Soogeun Park <bwmelon97@naver.com>
2020-03-14 23:35:39 +0900

    Update index.js

--- frontend/src/components/Structure/CommonComponents/index.js
+++ frontend/src/components/Structure/CommonComponents/index.js
@@ -1,3 +1,5 @@
 export { default as Logo } from './Logo';
 export { default as UserPhoto } from './UserPhoto';
 export { default as MoreButton } from './MoreButton';
+export { default as UnderLineTextArea } from './UnderLineTextArea'
+export { default as NoneOutlineTextArea } from './NoneOutlineTextArea'
