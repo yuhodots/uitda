423e65cb32c53f94508113e7172713e5f4fa7529
Soogeun Park <bwmelon97@naver.com>
2020-03-14 23:34:51 +0900

    Update calendar_consts.js

--- frontend/src/constants/calendar_consts.js
+++ frontend/src/constants/calendar_consts.js
@@ -9,3 +9,13 @@ export const GUEST_CLOSED = 'guest_closed';
 /* Carpool Calendar Menu */
 export const TOTAL = 'total';
 export const MY = 'my';
+
+/* Carpool Event Subtitle */
+export const DEPARTURE = 'departure';
+export const DESTINATION = 'destination';
+export const START = 'start';
+export const START_DATE = 'start_date';
+export const START_TIME = 'start_time';
+export const CONTACT = 'contact';
+export const MEETING_PLACE = 'meeting_place';
+export const DESCRIPTION = 'description';
