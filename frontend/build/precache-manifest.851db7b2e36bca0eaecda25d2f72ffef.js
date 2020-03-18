34e965447dfd88a5b7acd4be4ad10baca4ee4a57
Yuho Jeong <yuho8437@unist.ac.kr>
2020-03-18 23:40:54 +0900

    npm build

---
+++ frontend/build/precache-manifest.851db7b2e36bca0eaecda25d2f72ffef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac6aed6fc26b0fa93d2b3a4e274937f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7a0668e3425209dea9",
+    "url": "/static/css/2.774e23d0.chunk.css"
+  },
+  {
+    "revision": "0e9e113ad04afd581323",
+    "url": "/static/css/main.0ebe040a.chunk.css"
+  },
+  {
+    "revision": "ce7a0668e3425209dea9",
+    "url": "/static/js/2.6b7cc48e.chunk.js"
+  },
+  {
+    "revision": "50643862e8400d72ca5ddedd86e636b3",
+    "url": "/static/js/2.6b7cc48e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e9e113ad04afd581323",
+    "url": "/static/js/main.773eca51.chunk.js"
+  },
+  {
+    "revision": "30891cb8f3533e900f99",
+    "url": "/static/js/runtime-main.9d830519.js"
+  },
+  {
+    "revision": "e2fe49f7324cc2e2e4356112abbca592",
+    "url": "/static/media/Basic_img.e2fe49f7.jpg"
+  },
+  {
+    "revision": "98cdaa6842bdfd9d7dcb87ea183320f1",
+    "url": "/static/media/UI_uni.98cdaa68.png"
+  },
+  {
+    "revision": "0ebffe888457d84bafd9d44527b416db",
+    "url": "/static/media/User_Basic_img.0ebffe88.JPG"
+  },
+  {
+    "revision": "1c29d35f3dab221ccf4abf853a276405",
+    "url": "/static/media/carpool.1c29d35f.png"
+  },
+  {
+    "revision": "c356e5be2ca73ae721fef068f8e2a68a",
+    "url": "/static/media/carpool_preview.c356e5be.png"
+  },
+  {
+    "revision": "6aff96732c00f2d6f727849ecd29e028",
+    "url": "/static/media/market.6aff9673.png"
+  },
+  {
+    "revision": "b963d1d748328f85ed95acfe12b71f1e",
+    "url": "/static/media/market_preview.b963d1d7.png"
+  },
+  {
+    "revision": "15acfe20b9ffa33787880399d0059eb9",
+    "url": "/static/media/networking.15acfe20.png"
+  },
+  {
+    "revision": "4dd191500a7581bc9e6bdf8d67797ff1",
+    "url": "/static/media/networking_preview.4dd19150.png"
+  },
+  {
+    "revision": "5c440ee40ed0f9d1e1c0575dbdd12898",
+    "url": "/static/media/outlook.5c440ee4.png"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f02784fb0a8cc559f4a7247a69702ada.js"
+  "/precache-manifest.851db7b2e36bca0eaecda25d2f72ffef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
