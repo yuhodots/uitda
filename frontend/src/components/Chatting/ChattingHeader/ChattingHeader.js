b728f206b8a2cc5bca41361d15593295b1af654a
Soogeun Park <bwmelon97@naver.com>
2020-03-26 20:07:14 +0900

    Update ChattingHeader.js

--- frontend/src/components/Chatting/ChattingHeader/ChattingHeader.js
+++ frontend/src/components/Chatting/ChattingHeader/ChattingHeader.js
@@ -7,6 +7,8 @@ import LinkCategory from "../../Structure/BaseHeader/LinkCategory_MidComp";
 
 class ChattingHeader extends Component {
 
+    /* 헤더 가운데에 들어갈 컴포넌트를 render하는 함수
+       다른 게시판으로 이동하는 링크가 담긴 컴포넌트 */
     _renderMiddleComponent = () => {
 
         const categoryDatas = [
