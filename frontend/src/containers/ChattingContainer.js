81e8f14303dcc5841b7af7d803b85885aeb823c0
Soogeun Park <bwmelon97@naver.com>
2020-03-30 16:51:47 +0900

    계정관리, Logout 기능을 추가

--- frontend/src/containers/ChattingContainer.js
+++ frontend/src/containers/ChattingContainer.js
@@ -4,11 +4,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import SocketIo from 'socket.io-client';
 
-import { getStatusRequest } from "../store/actions/auth";
+import { getStatusRequest, logoutRequest } from "../store/actions/auth";
 import ChattingHeader from "../components/Chatting/ChattingHeader";
 import ChattingBody from "../components/Chatting/ChattingBody";
 
+const chattingSocket = SocketIo.connect('/chatting');
 
 class ChattingContainer extends Component {
 
@@ -28,7 +30,12 @@ class ChattingContainer extends Component {
     render() {
 
         const { isLoaded } = this.state;
-        const { curUser, isIndex } = this.props;
+        const { 
+            curUser, 
+            isIndex,
+        
+            logoutRequest
+        } = this.props;
 
 
         return(
@@ -37,7 +44,10 @@ class ChattingContainer extends Component {
                 curUser ?
 
                 <div style={{height: '100%', width: '100%'}}>
-                    <ChattingHeader />
+                    <ChattingHeader 
+                        curUser={curUser}
+                        logoutRequest={logoutRequest}
+                    />
                     <ChattingBody isIndex={isIndex} />
                 </div> :
 
@@ -66,6 +76,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         getStatusRequest: () => dispatch(getStatusRequest()),           // 현재 유저 정보를 불러오는 request 액션
+        logoutRequest: () => dispatch(logoutRequest()),                 // 로그아웃 GET request 액션
     }
 }
 
