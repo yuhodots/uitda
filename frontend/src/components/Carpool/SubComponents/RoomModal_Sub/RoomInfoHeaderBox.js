3bf3ea8964c1ee8cbb681584c8487e1560810ec3
Soogeun Park <bwmelon97@naver.com>
2020-04-03 19:34:48 +0900

    Carpool Room Modal의 Popover Content 코드 리펙토링

--- frontend/src/components/Carpool/SubComponents/RoomModal_Sub/RoomInfoHeaderBox.js
+++ frontend/src/components/Carpool/SubComponents/RoomModal_Sub/RoomInfoHeaderBox.js
@@ -4,10 +4,9 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Button } from "antd";
-import { 
-    DeleteOutlined, EditOutlined, FieldTimeOutlined, CloseCircleOutlined, PushpinOutlined, MoreOutlined
-} from "@ant-design/icons";
+import { MoreOutlined } from "@ant-design/icons";
 
+import renderPopoverContent from "./renderPopoverContent";
 import { UserPhoto, UitdaPopover } from "../../../Structure/CommonComponents";
 import { colors } from "../../../../styles/variables";
 import { 
@@ -64,7 +63,8 @@ const HeaderInfoBox = styled.div`
             margin-right: 1rem;
 
             color: ${colors.font_gray};
-            
+            cursor: default;
+
             display: flex;
             flex-flow: row nowrap;
             align-items: center;
@@ -100,8 +100,6 @@ const HeaderInfoBox = styled.div`
 /* React Component */
 class RoomInfoHeaderBox extends Component {
     
-
-
     /* label에 해당하는 색과 텍스트를 리턴하는 메서드 */
     _getColorAndText = () => {
         
@@ -117,89 +115,6 @@ class RoomInfoHeaderBox extends Component {
             default:  return {labelColor: colors.closed_gray, labelText: '마감'}
         }
     }
-
-    /* label에 따라 그에 해당되는 더보기 content를 생성하는 함수 */
-    _renderContent = () => {
-
-        const {
-            curUser,
-            selectedEvent,
-            deleteEvent, changeModeToUpdate,
-            closeOrCancleEvent,
-            joinEvent, cancleJoinEvent
-        } = this.props;
-
-        const { id, label, guestlist } = selectedEvent
-
-        let owenrContentList = [
-            {
-                icon: <EditOutlined />,
-                text: '수정하기',
-                clickMethod: () => {
-                    changeModeToUpdate();
-                }
-            },
-            {
-                icon: <DeleteOutlined />,
-                text: '삭제하기',
-                clickMethod: () => {
-                    deleteEvent(id);
-                    
-                    /* 현재는 시간 조금 이후에 새로고침이지만,
-                       socket으로 구현이 되면, modal 닫기만 하면 된다. */
-                    setTimeout(() => {
-                        window.location.reload();
-                    }, 300);
-                }
-            }
-        ]
-
-        switch (label) {
-
-            case OWNER:
-                owenrContentList = [...owenrContentList, {
-                    icon: <FieldTimeOutlined />,
-                    text: '마감하기',
-                    clickMethod: () => {
-                        closeOrCancleEvent(id, 2)   // 마감
-                    }   
-                }]
-                return owenrContentList;
-            case OWNER_CLOSED:
-                owenrContentList = [...owenrContentList, {
-                    icon: <CloseCircleOutlined  />,
-                    text: '마감 취소하기',
-                    clickMethod: () => {
-                        closeOrCancleEvent(id, 1)   // 모집 중
-                    }   
-                }]
-                return owenrContentList;
-
-            case GUEST:
-            case GUEST_CLOSED:
-                return [
-                    {
-                        icon: <CloseCircleOutlined  />,
-                        text: '내 일정에서 제외하기',
-                        clickMethod: () => {
-                            const foundedGuest = guestlist.filter(guest => guest.email === curUser.email);
-                            cancleJoinEvent(foundedGuest.id);
-                        }
-                    }
-                ]
-
-            default:
-                return [
-                    {
-                        icon: <PushpinOutlined />,
-                        text: '내 일정에 추가하기',
-                        clickMethod: () => {
-                            joinEvent(id)
-                        }
-                    }
-                ] 
-        }
-    } 
 
     _handleUpdate = () => {
         const {
@@ -219,13 +134,24 @@ class RoomInfoHeaderBox extends Component {
 
         const {
             isUpdateMode, 
+            
+            curUser,
             selectedEvent,
+
             changeModeToRead,
+            deleteEvent, changeModeToUpdate,
+            closeOrCancleEvent,
+            joinEvent, cancleJoinEvent
         } = this.props
 
         const { user, created } = selectedEvent
 
-        const MoreButtonContentList = this._renderContent();
+        const clickMethods = {
+            deleteEvent, changeModeToUpdate,
+            closeOrCancleEvent,
+            joinEvent, cancleJoinEvent
+        };
+        const MoreButtonContentList = renderPopoverContent(curUser, selectedEvent, clickMethods);
 
         const { labelColor, labelText } = this._getColorAndText();
 
---
+++ frontend/src/components/Carpool/SubComponents/RoomModal_Sub/renderPopoverContent.js
@@ -0,0 +1,101 @@
+/* 카풀 Room 모달의 헤더 더보기에 해당하는 Popover content의 data list를 만들어주는 함수 */
+
+import React from 'react';
+import { 
+    DeleteOutlined, EditOutlined, FieldTimeOutlined, CloseCircleOutlined, PushpinOutlined, MessageOutlined
+} from "@ant-design/icons";
+
+import { OWNER, OWNER_CLOSED, GUEST, GUEST_CLOSED } from "../../../../constants/calendar_consts";
+
+
+const renderCarpoolModalPopoverContent = ( curUser, selectedEvent, clickMethods ) => {
+
+    const { id, label, guestlist, user } = selectedEvent;
+    const { 
+        deleteEvent, changeModeToUpdate,
+        closeOrCancleEvent,
+        joinEvent, cancleJoinEvent
+    } = clickMethods;
+
+    /* 방장, 방장 마감의 공통 기능 (수정, 삭제) */
+    let ownerContentList = [
+        {
+            icon: <EditOutlined />,
+            text: '수정하기',
+            clickMethod: () => { changeModeToUpdate(); }
+        },
+        {
+            icon: <DeleteOutlined />,
+            text: '삭제하기',
+            clickMethod: () => {
+                deleteEvent(id);
+                
+                /* 현재는 시간 조금 이후에 새로고침이지만,
+                   socket으로 구현이 되면, modal 닫기만 하면 된다. */
+                setTimeout(() => {
+                    window.location.reload();
+                }, 300);
+            }
+        }
+    ];
+
+    /* 방장이 아닌 방의 공통 기능 (메시지 보내기) */
+    let guestContentList = [
+        {
+            icon: <MessageOutlined />,
+            text: '메시지 보내기',
+            clickMethod: () => { window.location.assign(`/chatting/t/${user.id}`) }
+        }
+    ]
+
+
+    switch (label) {
+
+        case OWNER:
+            return [
+                ...ownerContentList,
+                {
+                    icon: <FieldTimeOutlined />,
+                    text: '마감하기',
+                    clickMethod: () => { closeOrCancleEvent(id, 2) /* 마감 */ }   
+                }
+            ]
+
+        case OWNER_CLOSED:
+            return [
+                ...ownerContentList,
+                {
+                    icon: <CloseCircleOutlined  />,
+                    text: '마감 취소하기',
+                    clickMethod: () => { closeOrCancleEvent(id, 1)   /* 모집 중 */  }   
+                }
+            ]
+
+        case GUEST:
+        case GUEST_CLOSED:
+            return [
+                {
+                    icon: <CloseCircleOutlined  />,
+                    text: '내 일정에서 제외하기',
+                    clickMethod: () => {
+                        const foundedGuest = guestlist.filter(guest => guest.email === curUser.email);
+                        cancleJoinEvent(foundedGuest.id);
+                    }
+                },
+                ...guestContentList
+            ]
+
+        default:
+            return [
+                {
+                    icon: <PushpinOutlined />,
+                    text: '내 일정에 추가하기',
+                    clickMethod: () => { joinEvent(id) }
+                },
+                ...guestContentList
+            ] 
+    }
+}
+
+
+export default renderCarpoolModalPopoverContent;
