17d32c90e90d74e4291125b1b3940e7a5cc30841
Soogeun Park <bwmelon97@naver.com>
2020-04-23 20:01:35 +0900

    PhotoBox에 ant design의 carousel을 적용

--- frontend/src/components/Board/BoardBody/SubComponents/PostCard/Cards/CardCompoents/PhotoBox.js
+++ frontend/src/components/Board/BoardBody/SubComponents/PostCard/Cards/CardCompoents/PhotoBox.js
@@ -1,135 +1,126 @@
-// 상위 컴포넌트: MarketCard, NetworkCard
+// 상위 컴포넌트: PhotoCard
 
-import React, {Component} from 'react';
-import styled from 'styled-components';
+import React, { Component, createRef } from 'react';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Carousel } from "antd";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 
-import basicImg from './Basic_img.jpg';
+import { BoardBasicImage as BasicImg } from '../../../../../../../styles/images';
 
 /* Styled-Components */
-
-const BaseDiv = styled.div`
-    width: 100%;
-`
-
+/* PhotoBox 전체 영역 div 태그 */
 const PhotoWrapper = styled.div`
     width: 100%;
     height: 0;
     padding-top: 70%;
     position: relative;
-    display: block;
     overflow: hidden;
 `;
 
-const PhotoLink = styled.a`
-    width: 100%;
-    height: 0;
-    padding-top: 70%;
-    position: absolute;
-    top: 0;
-    display: block;
-    overflow: hidden;
+    /* PhotoBox 클릭 시 해당 페이지로 넘겨주는 Link Wrapper */
+    const PhotoLink = styled(Link)`
+        width: 100%;
+        padding-top: 70%;
+        position: absolute;
+        top: 0;
+        overflow: hidden;
 
-    background-image: ${props => 
-        props.hasPhoto ? 'none' : `url(${basicImg})`};
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-`;
+        /* 사진이 없는 경우, Basic Image를 띄움 */
+        background-image: ${props => 
+            props.hasPhoto ? 'none' : `url(${BasicImg})`};
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
+    `;
 
-const ImgList = styled.div`
-    width: ${props => props.width};
-    height: ${props => props.height};
-    position: absolute;
-    top: 0;
-    left: ${props => props.posLeft};
+        /* 사진을 담는 박스 padding이 아닌 실제 크기를 갖도록 함. */
+        const PhotoContainer = styled.div`
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        `;
 
-    display: flex;
-    flex-flow: row nowrap;
-`;
+            /* 사진 이미지 Item */
+            const PhotoItem = styled.div`
+                height: ${props => props.imgHeight}px;
+                width: 100%;
 
-const ImgItem = styled.div`
-    height: 100%;
-    flex: 1;    
+                background-image: ${props => `url(${props.imgURL})`};
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-size: cover;
+            `;
 
-    background-image: ${props => `url(${props.imgURL})`};
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-`;
+    /* 이미지에 음영 효과를 주기 위한 마스크 */
+    const BlackMask = styled.div`
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.25);
+        opacity: 1;
+        transition: opacity 0.25s;
 
-const BlackMask = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.25);
-    opacity: 1;
-    transition: opacity 0.25s;
+        /* Basic 이미지인 경우에만 hover 효과를 줌 */
+        ${props => !props.hasPhoto && css`
+            :hover {
+                opacity: 0.5;
+            }
+        `}
+    `;
 
-    :hover {
-        opacity: 0.5;
-    }
-`;
+    /* 사진 앞, 뒤 이동 버튼 Container */
+    const ButtonContainer = styled.div`
+        position: absolute;
+        top: ${props => props.posTop};
+        width: 100%;
+    `;
 
-const ButtonContainer = styled.div`
-    position: absolute;
-    top: ${props => props.posTop};
-    width: 100%;
-`;
+        /* 버튼 공통 스타일 */
+        const PhotoButton = styled.div`
+            position: absolute;
+            height: 2.5rem;
+            width: 2.5rem;
+            border-radius: 50%;
 
-const PrevButton = styled.button`
-    height: 2.5rem;
-    width: 2.5rem;
-    border-radius: 50%;
+            background-color: white;
 
-    position: absolute;
-    left: 1rem;
+            font-weight: bold;
 
-    background-color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
-    display: ${props => {
-        return !(props.isButtonOn) && 'none' 
-    }};
+            :hover {
+                top: -0.125rem;
+                height: 2.75rem;
+                width: 2.75rem;
+                box-shadow: 0 0 10px rgba(0,0,0,.3);
+            }
+            cursor: pointer;
+        `;
 
-    :hover {
-        left: .875rem;
-        top: -0.125rem;
-        height: 2.75rem;
-        width: 2.75rem;
-        box-shadow: 0 0 10px rgba(0,0,0,.3);
-    }
-`;
+        /* 이전 사진 버튼 */
+        const PrevButton = styled(PhotoButton)`
+            left: 1rem;
+            :hover {
+                left: .875rem;
+            }
+        `;
 
-const NextButton = styled.button`
-    height: 2.5rem;
-    width: 2.5rem;
-    border-radius: 50%;
+        /* 다음 사진 버튼 */
+        const NextButton = styled(PhotoButton)`
+            right: 1rem;
+            :hover {
+                right: .875rem;
+            }
+        `;
 
-    position: absolute;
-    right: 1rem;
-
-    display: ${props => {
-        return !(props.isButtonOn) && 'none' 
-    }};
-
-    background-color: white;
-
-    :hover {
-        right: .875rem;
-        top: -0.125rem;
-        height: 2.75rem;
-        width: 2.75rem;
-        box-shadow: 0 0 10px rgba(0,0,0,.3);
-    }
-`;
-
-const CircleContainer = styled.div`
-`;
-
-const CircleButton = styled.button`
-`;
 
 /////////////////////////////
 
@@ -138,120 +129,91 @@ class PhotoBox extends Component {
 
     state = {}
 
+    photoBoxRef = createRef();  // 박스 전체 ref
+    carouselRef = createRef();  // ant design carousel (사진 넘기기 박스) ref
+
     componentDidMount() {
-        this.setState({
-            photoOrder: 0,
-            containerWidth: this.container.offsetWidth,
-            isHover: false
-        })
-        window.addEventListener('resize', this._handleResize)       // 브라우저 창(window)의 크기가 변화되는 이벤트를 받음.
+        window.addEventListener('resize', this._handleResize);                          // 브라우저 창(window)의 크기가 변화되는 이벤트를 받음.
+        this.photoBoxRef.current.addEventListener('mouseover', this._handleMouseEnter);
+        this.photoBoxRef.current.addEventListener('mouseleave', this._handleMouseLeave);
+        this._handleResize();
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this._handleResize)
+        window.removeEventListener('resize', this._handleResize);
+        this.photoBoxRef.current.removeEventListener('mouseenter', this._handleMouseEnter);
+        this.photoBoxRef.current.removeEventListener('mouseleave', this._handleMouseLeave);
     }
 
+    /* PhotoBox width 사이즈 업데이트 */
     _handleResize = (e) => {
         this.setState({
             ...this.state,
-            containerWidth: this.container.offsetWidth,
+            containerWidth: this.photoBoxRef.current.offsetWidth,
         })
     }
 
-    _handleHover = (e) => {
-
-    }
-
-    _ClickPrevButton = () => {
-        this.setState({
-            photoOrder: this.state.photoOrder - 1
-        })
-    }
-
-    _ClickNextButton = () => {
-        this.setState({
-            photoOrder: this.state.photoOrder + 1
-        })
-    }
-
-    _ClickCircleButton = (i) => {
-        console.log(i);
+    /* PhotoBox에 마우스를 올리면 isHover true */
+    _handleMouseEnter = (e) => {
         this.setState({
             ...this.state,
-            photoOrder: i
+            isHover: true,
         })
     }
 
-    _renderContents() {
-        const { filelist, boardName, postId } = this.props;
-        const { photoOrder, containerWidth, isHover } = this.state;
-        let hasPhoto = filelist.length !== 0;
+    /* PhotoBox에서 마우스가 떠나면 isHover false */
+    _handleMouseLeave = (e) => {
+        this.setState({
+            ...this.state,
+            isHover: false,
+        })
+    }
 
-        let listWidth = containerWidth * filelist.length;
-        let imgHeight = containerWidth * 0.7;
-
-        let listPosLeft = -(containerWidth * photoOrder);
-
-        let buttonPosTop = (imgHeight / 2) - 18;
-
-        let isPrevButtonOn = hasPhoto && (photoOrder !== 0);
-        let isNextButtonOn = hasPhoto && (photoOrder !== (filelist.length - 1));
-
-        let postURL = `/board/${boardName}/${postId}`;
-
-        return (
-            <PhotoWrapper>
-                <PhotoLink hasPhoto={hasPhoto} href={postURL}>
-                    {
-                        // 사진 리스트
-                        hasPhoto &&
-                        <ImgList 
-                            width={`${listWidth}px`} 
-                            height={`${imgHeight}px`} 
-                            posLeft={`${listPosLeft}px`} 
-                            ref={ref => {this.imgListDOM = ref}}>
-                            
-                            {filelist.map((file, id) => {
-                                return <ImgItem imgURL={file.location} key={id} />
-                            })}
-                        </ImgList> 
-                    }
-                    {/* 사진이 있든 없든 공통적으로 BlackMask 가짐 */}
-                    <BlackMask/>
-                </PhotoLink>
-                {
-                    // 사진 넘기기 버튼
-                    // filelist.length > 1 &&
-                    <ButtonContainer isHover={isHover} posTop={`${buttonPosTop}px`} >
-                        <PrevButton isButtonOn={isPrevButtonOn} onClick={this._ClickPrevButton} />
-                        <NextButton isButtonOn={isNextButtonOn} onClick={this._ClickNextButton} />
-                    </ButtonContainer> 
-                }
-                {
-                    // 사진 순서 표시하는 동글뺑이들
-                    filelist.length > 1 &&
-                    <CircleContainer>
-                        {filelist.map((f, i) => {
-                            let isHighLight = photoOrder === i;
-                            return <CircleButton 
-                                isHighLight={isHighLight} 
-                                onClick={(i) => {this._ClickCircleButton(i)}}
-                                key={i}/>
-                        })}
-                    </CircleContainer> 
-                }
-            </PhotoWrapper>
-            
-        )
+    /* filelist를 가지고, PhotoItem을 render하는 메서드 */
+    _renderPhoto = (filelist, imgHeight) => {
+        return filelist.map( file => {
+            const { id, location } = file;
+            return <PhotoItem imgURL={location} imgHeight={imgHeight} key={id} />
+        } )
     }
 
     render() {
-        const {containerWidth} = this.state;
+
+        const { filelist, boardName, postId } = this.props;
+        const { containerWidth, isHover } = this.state;
+
+        const hasPhoto = filelist.length !== 0;             // 사진이 있는 글 인지
+        const imgHeight = containerWidth * 0.7;             // 이미지 세로 크기 (가로 길이 * 0.7)
+        const buttonPosTop = (imgHeight / 2) - 18;          // absolute postion인 Button Container를 세로 가운데에 위치하기 위함
+        const postURL = `/board/${boardName}/${postId}`;    // 게시글 url
 
         return (
-            <BaseDiv ref={ref => {this.container = ref}}>
-                {containerWidth && this._renderContents()}
-            </BaseDiv>
+            <PhotoWrapper ref={this.photoBoxRef} >
+                <PhotoLink hasPhoto={hasPhoto} to={postURL}>
+                    {
+                        hasPhoto &&
+                        
+                        <PhotoContainer>
+                            <Carousel ref={this.carouselRef} >
+                            {
+                                this._renderPhoto(filelist, imgHeight)
+                            }
+                            </Carousel>
+                        </PhotoContainer>
+
+                    }
+                    <BlackMask hasPhoto={hasPhoto} />
+                </PhotoLink>
+
+                {
+                    /* 사진 넘기기 버튼 */
+                    filelist.length > 1 && isHover &&
+                    <ButtonContainer posTop={`${buttonPosTop}px`} >
+                        <PrevButton onClick={ () => this.carouselRef.current.prev() } > <LeftOutlined /> </PrevButton>
+                        <NextButton onClick={ () => this.carouselRef.current.next() } > <RightOutlined /> </NextButton>
+                    </ButtonContainer> 
+                }
+            </PhotoWrapper>
         )
     }
 }
