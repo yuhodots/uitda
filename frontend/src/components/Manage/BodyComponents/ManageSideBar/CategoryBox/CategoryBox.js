8e3ceaebba65bbe20a6e57cb7b571f100fc25400
Soogeun Park <bwmelon97@unist.ac.kr>
2020-05-10 01:50:26 +0900

    Manage Side Category Box 카테고리 수정 / 스타일링 작업

--- frontend/src/components/Manage/BodyComponents/ManageSideBar/CategoryBox/CategoryBox.js
+++ frontend/src/components/Manage/BodyComponents/ManageSideBar/CategoryBox/CategoryBox.js
@@ -1,7 +1,7 @@
 // 상위 컴포넌트: ManageSideBox
 
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom'
@@ -10,12 +10,12 @@ import { colors } from '../../../../../styles/variables'
 import { BoxTemplate } from '../../../../../styles/templates/manage'
 
 import {
-    // MANAGE_ACCOUNT,
+    MANAGE_PROFILE,
     MANAGE_POSTS_MARKET,
     MANAGE_POSTS_NETWORKING,
     MANAGE_COMMENTS,
     MANAGE_LIKEPOSTS,
-    // MANAGE_CONTACT
+    MANAGE_CONTACT
 } from '../../../../../constants/manage_category'
 
 
@@ -24,94 +24,144 @@ import {
 /* Category 박스 전체를 의미 */
 const BoxArea = styled(BoxTemplate)`
     margin: 0;
-    padding: 1.5rem;
+    padding: 0;
 
     width: 100%;
-    height: 32rem;
 
     display: flex;
     flex-flow: column nowrap;
 `;
 
-/* 카테고리 그룹 */
-const SubGroup = styled.div`
-    margin-bottom: 2rem;
+    /* 카테고리 그룹 */
+    const SubGroup = styled.div`
+        padding: 0.5rem 0;
 
-    display: flex;
-    flex-flow: column nowrap;
-`
+        border-bottom: 1px solid ${colors.gray_bg};
+        ${props => props.isBottom && css`
+            border-bottom: none;
+        `}
 
-/* 카테고리 그룹 제목 */
-const SubTitle = styled.div`
-    margin-bottom: 1rem;
+        display: flex;
+        flex-flow: column nowrap;
+    `;
 
-    font-size: 1rem;
-`;
+        /* 카테고리 Item 공통 속성
+           좌측에 동일한 Padding 값을 갖도록 함 */
+        const CategoryItemStyle = css`
+            position: relative;
+            padding: 0.25rem 0;
+            padding-left: 2.5rem;
 
-/* 카테고리 링크 */
-const CategoryLink = styled(Link)`
-    text-decoration: none;
-    color: ${ (props) => {
-        return props.isHighLight ?
-        colors.font_darkgray :
-        colors.font_lightgray
-    }};
-    margin-bottom: 0.5rem;
+            white-space: nowrap;
+        `;
 
-    padding-left: 0.5rem;
-    font-size: 0.9375rem;
-`;
+        /* Link 태그 공통 스타일
+           밑줄을 없애고, hover 시 자동 폰트 색 변환을 없앴으며,
+           hover 시 배경색을 입히는 효과 추가 */
+        const LinkStyle = css`
+            ${CategoryItemStyle}
+            
+            text-decoration: none;
+            color: ${ props => props.color };
+
+            :hover {
+                color: ${ props => props.color };
+                background-color: ${colors.gray_bg};
+            }
+        `;
+
+        /* SubGroup의 Title 공통 속성. font 스타일링 */
+        const TitleStyle = css`
+            ${CategoryItemStyle}
+
+            /* color: ${props => props.color}; */
+            color: ${colors.font_darkgray};
+            font-size: 1rem;
+        `;
+
+            /* Link 기능을 하는 Title 태그 */
+            const TitleLink = styled(Link)`
+                ${TitleStyle}
+                ${LinkStyle}
+            `;
+
+            /* Link 기능을 하는 세부 항목 태그 */
+            const SubLink = styled(Link)`
+                ${LinkStyle}
+                font-size: 0.875rem;
+            `;
+
+            /* Link 기능이 없는 Title 태그 */
+            const GroupTitle = styled.div`
+                ${TitleStyle}
+                cursor: default;
+            `;
 
 
 /* React Component */
+const CategoryBox = ({kind}) => {
 
-class CategoryBox extends Component {
+    const hightLightColor = colors.blue;
+    const defaultTitleColor = colors.font_darkgray;
+    const defaultSubColor = colors.font_gray;
 
-
-    render () {
-
-        const { kind } = this.props;
-
-        return (
-            <BoxArea>
-                <SubGroup>
-                    <SubTitle>작성한 글 관리</SubTitle>
-                    <CategoryLink 
-                        to='/manage/posts/market'
-                        isHighLight={kind === MANAGE_POSTS_MARKET}
-                    >
-                        다판다 글 관리
-                    </CategoryLink>
-                    <CategoryLink 
-                        to='/manage/posts/networking'
-                        isHighLight={kind === MANAGE_POSTS_NETWORKING}
-                    >
-                        잉력시장 글 관리
-                    </CategoryLink>
-                </SubGroup>
-                
-                
-                <SubGroup>
-                    <SubTitle>관심 글 목록</SubTitle>
-                    <CategoryLink 
-                        to='/manage/comments'
-                        isHighLight={kind === MANAGE_COMMENTS}
-                    >
-                        댓글을 단 게시글
-                    </CategoryLink>
-                    <CategoryLink 
-                        to='/manage/likeposts'
-                        isHighLight={kind === MANAGE_LIKEPOSTS}
-                    >
-                        관심있음 표시한 게시글
-                    </CategoryLink>
-                </SubGroup>
-                
-
-                <SubTitle>지난 알림 보기</SubTitle>
-            </BoxArea>
-        )
-    }
+    return (
+        <BoxArea>
+            <SubGroup>
+                <TitleLink
+                    to='/manage/profile' 
+                    color={kind === MANAGE_PROFILE ? hightLightColor : defaultTitleColor}
+                >
+                    회원정보 관리
+                </TitleLink>
+            </SubGroup>
+            
+            <SubGroup>
+                <GroupTitle color={kind === MANAGE_POSTS_MARKET || kind === MANAGE_POSTS_NETWORKING ? hightLightColor : defaultTitleColor} >
+                    작성한 글 관리
+                </GroupTitle>
+                <SubLink 
+                    to='/manage/posts/market'
+                    color={kind === MANAGE_POSTS_MARKET ? hightLightColor : defaultSubColor}
+                >
+                    다판다 글 관리
+                </SubLink>
+                <SubLink 
+                    to='/manage/posts/networking'
+                    color={kind === MANAGE_POSTS_NETWORKING ? hightLightColor : defaultSubColor}
+                >
+                    잉력시장 글 관리
+                </SubLink>
+            </SubGroup>
+            
+            <SubGroup>
+                <GroupTitle color={kind === MANAGE_COMMENTS || kind === MANAGE_LIKEPOSTS ? hightLightColor : defaultTitleColor} >
+                    관심 글 목록
+                </GroupTitle>
+                <SubLink 
+                    to='/manage/comments'
+                    color={kind === MANAGE_COMMENTS ? hightLightColor : defaultSubColor}
+                >
+                    댓글을 단 게시글
+                </SubLink>
+                <SubLink 
+                    to='/manage/likeposts'
+                    color={kind === MANAGE_LIKEPOSTS ? hightLightColor : defaultSubColor}
+                >
+                    관심있음 표시한 게시글
+                </SubLink>
+            </SubGroup>
+            
+            <SubGroup isBottom={true} >
+                <TitleLink 
+                    to='/manage/contact'
+                    color={kind === MANAGE_CONTACT ? hightLightColor : defaultTitleColor}
+                >
+                    문의하기
+                </TitleLink>
+            </SubGroup>
+        </BoxArea>
+    )
 }
 
 CategoryBox.propTypes = {
