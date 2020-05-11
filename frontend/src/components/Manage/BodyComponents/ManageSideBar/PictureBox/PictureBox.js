dde85c1661d5cc8d16d8b77d6caf550d1c1ab66e
Soogeun Park <bwmelon97@unist.ac.kr>
2020-05-11 17:13:24 +0900

    Picture Box의 사진 upload를 단순히 display만 하는 컴포넌트로 변경

--- frontend/src/components/Manage/BodyComponents/ManageSideBar/PictureBox/PictureBox.js
+++ frontend/src/components/Manage/BodyComponents/ManageSideBar/PictureBox/PictureBox.js
@@ -1,78 +1,66 @@
 
 
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Upload } from "antd";
 
 import { UserPhoto } from "../../../../Structure/CommonComponents";
+import { BorderBoxStyle } from "../../../../../styles/CommonCSS";
 import { colors } from '../../../../../styles/variables'
-import { BoxTemplate } from '../../../../../styles/templates/manage'
 
 /* Styled Components */
-const BoxArea = styled(BoxTemplate)`
+const BoxArea = styled.div`
     margin-bottom: 2rem;
     padding: 1rem;
 
     width: 100%;
     height: 16rem;
 
+    ${BorderBoxStyle}
+
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
+    justify-content: center;
 `;
 
     /* 사진이 들어가는 동그란 영역 */
-    const PhotoUploadBox = styled.div`
+    const UserPhotoBox = styled.div`
         margin-bottom: 1rem;
         width: 8rem;
         height: 8rem;
         border-radius: 50%;
+        border: 1px solid ${colors.gray_line};
 
         overflow: hidden;
+    `;
+
+    /* User Email */
+    const Email = styled.div`
+        font-size: 0.875rem;
+        color: ${colors.font_gray};
+        cursor: default;
     `;
 
     /* User Name이 들어가는 공간 */
     const UserName = styled.div`
         margin-top: 0.5rem;
         font-size: 1.125rem;
-    `;
-
-    const Email = styled.div`
-        /* margin-top: 0.5rem; */
-        font-size: 0.875rem;
-        color: ${colors.font_gray};
+        cursor: default;
     `;
 
 
+const PictureBox = ({curUser}) => {
 
-class PictureBox extends Component {
-
-
-
-    render () {
-
-        const {
-            curUser
-        } = this.props
-
-        const uploadProps = {
-            name: 'userfile',
-            action: '/api/users/update',
-        }
-
-        return (
-            <BoxArea>
-                <PhotoUploadBox>
-                    <Upload {...uploadProps} >
-                        <UserPhoto size={128} imgURL={curUser.pic_location} />
-                    </Upload>
-                </PhotoUploadBox>
-                <Email>{curUser.email}</Email>
-                <UserName>{curUser.username}</UserName>
-            </BoxArea>
-        )
-    }
+    return (
+        <BoxArea>
+            <UserPhotoBox>
+                <UserPhoto size={128} imgURL={curUser.pic_location} />
+            </UserPhotoBox>
+            <Email>{curUser.email}</Email>
+            <UserName>{curUser.username}</UserName>
+        </BoxArea>
+    )
 }
 
 PictureBox.propTypes = {
