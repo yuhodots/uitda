3808f3c5224b31105af52e2046a7b82d7e694f6b
Soogeun Park <bwmelon97@naver.com>
2020-06-07 17:07:23 +0900

    UitdaPhotoCarousel 생성

--- frontend/src/components/Home/EnterPageSubComponents/index.js
+++ frontend/src/components/Home/EnterPageSubComponents/index.js
@@ -1,3 +1,2 @@
 export { default as OutlookLoginBox } from './OutlookLoginBox'
 export { default as CategoryInfoBox } from './CategoryInfoBox'
-export { default as PhotoListBox } from './PhotoListBox'
